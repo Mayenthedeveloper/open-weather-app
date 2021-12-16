afa51cfbc090116b4b3a5b4ed2d3bd4168acb453
Mayen Akpan <neroxma@yahoo.com>
2021-12-15 19:32:46 -0800

    Updated weatherDetail

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -12,8 +12,22 @@ function WeatherDetails() {
             <span>23.99&deg;</span>
           </div>
           <div className="description">
-            <div className="weatherCondition">sunnyy</div>
+            <div className="weatherCondition">sunny</div>
             <div className="place">Mumbai, IN</div>
+          </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                6:30 PM <br />
+                Sunset
+              </p>
+            </div>
           </div>
         </div>
       </article>
